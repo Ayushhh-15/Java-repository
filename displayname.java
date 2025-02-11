605d17462ea9474fd0fa455439b165f9f0a1b736
Ayush Rawat <ayush.rwt15@gmail.com>
2025-02-11 22:36:15 +0530

    Add files via upload

---
+++ displayname.java
@@ -0,0 +1,7 @@
+public class displayname {
+    public static void main(String[] args) {
+        System.out.println("My name is Ayush");
+        System.out.println("Address: House no.1, Alaknanda colonny, Kotdwara, Uttarakhand");
+    }
+    
+}
---
+++ gradesheet.java
@@ -0,0 +1,19 @@
+public class gradesheet {
+    public static void main(String[] args) {
+        // Print the header of the grade sheet
+        System.out.println("*************************************************************");
+        System.out.println("\t\t   GRADE SHEET");
+        System.out.println("*************************************************************\n");
+
+        System.out.println("| Student ID \t| Name     \t| Subject      \t| Grade |");
+        System.out.println("------------------------------------------------------------");
+        
+        // Student records
+        System.out.println("| 01          \t| Ayush    \t| Math         \t| F     |");
+        System.out.println("| 02          \t| Ashmeet  \t| Science      \t| A+    |");
+        System.out.println("| 03          \t| Ishan    \t| English      \t| F     |");
+        System.out.println("| 04          \t| Krishna  \t| Chemistry    \t| O     |");
+        System.out.println("| 05          \t| Aman     \t| History      \t| C     |");
+        System.out.println("------------------------------------------------------------\n");
+    }    
+}
